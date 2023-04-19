7a3cd9e11780f69cd3ba34f9b25fc5c9049c33e7
Clinton Wee <Clinton.Wee@softwire.com>
2023-04-19 15:38:37 +0100

    First Draft

--- main.py
+++ main.py
@@ -3,21 +3,45 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-class xint (int):
+class FizzInt (int):
     def isMultipleOf(self, value):
         return self % value == 0
 
+def insertBeforeChar(character, oldWord, newWord):
+    index = oldWord.find(character)
+    if index == -1:
+        return oldWord + newWord
+    else:
+        final = oldWord[:index] + newWord + oldWord[index:]
+        return final
+
 def fizzbuzz():
     # Use a breakpoint in the code line below to debug your script.
-    total = xint(input(f'Hi! Please input the number you wish to count up to:'))
+    total = FizzInt(input(f'Hi! Please input the number you wish to count up to:'))
     for i in range(1, total + 1, 1):
-        curr = xint(i)
-        if(curr.isMultipleOf(5)):
-            print("Multiple Of 5")
-        elif(curr.isMultipleOf(3)):
-            print("Multiple Of 3")
-        else:
+        curr = FizzInt(i)
+        final = ""
+
+        if curr.isMultipleOf(3):
+            final += "Fizz"
+
+        if curr.isMultipleOf(5):
+            final += "Buzz"
+
+        if curr.isMultipleOf(11):
+            final = "Bong"
+
+        if curr.isMultipleOf(13):
+            final = insertBeforeChar("B", final, "Fezz")
+
+        if curr.isMultipleOf(17):
+            final = final[::-1]
+
+        if len(final) == 0:
             print(curr)
+            continue
+
+        print(final)
 
     # Press Ctrl+F8 to toggle the breakpoint.
 
