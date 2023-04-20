53ab7de1ee56281c779c04e74663922983a8e503
Clinton Wee <Clinton.Wee@softwire.com>
2023-04-20 09:11:43 +0100

    Added command line help

--- main.py
+++ main.py
@@ -85,6 +85,34 @@ def runCustomRules(args):
         rule.makeRule(ruleType, multiple, word, insertBeforeChar)
     rule.printResult()
 
+def printInstructions():
+    commands = [
+        ['TYPE', 'EXAMPLE', 'EXPLANATION'],
+        ['normal', 'normal-4-fizz', 'For multiples of 4, add "Fizz" to output'],
+        ['override', 'override-3-bong', 'For multiples of 3, output "Bong" ONLY'],
+        ['insertBefore', 'insertBefore-7-fezz-h', 'For multiples of 7, add "Fezz" to output, immediately before "H"'],
+        ['reverse', 'reverse-11', 'For multiples of 11, reverse output'],
+
+    ]
+    examples = [
+        ['COMMAND', 'NUMBER', 'RESULT'],
+        ['normal-3-fizz normal-5-buzz', '15', 'FizzBuzz'],
+        ['normal-2-fizz override-7-bong', '42', 'Bong'],
+        ['normal-3-buzz insertBefore-7-fezz-b', '21', 'FezzBuzz'],
+        ['normal-5-fizz normal-7-buzz reverse-10', '70', 'BuzzFizz'],
+    ]
+
+    print("\n\nInstructions: List out commands in the following format: [type]-[number]-[word]-[character]")
+    print("** Note that the rules generated will be carried out in sequence,from left to right \n")
+    widths = [max(map(len, col)) for col in zip(*commands)]
+    for row in commands:
+        print("  ".join((val.ljust(width) for val, width in zip(row, widths))))
+
+    print("\n\nSome Examples: \n")
+    widths = [max(map(len, col)) for col in zip(*examples)]
+    for row in examples:
+        print("  ".join((val.ljust(width) for val, width in zip(row, widths))))
+
 def fizzbuzz():
     args = sys.argv[1:]
     if len(args) == 0:
@@ -95,11 +123,11 @@ def fizzbuzz():
             runDefaultRules()
         else:
             print("GAME ABORTED")
-            return
+    elif len(args) == 1 and args[0] == 'help':
+        printInstructions()
     else:
         runCustomRules(args)
-    print("END OF GAME")
-
+        print("END OF GAME")
 
 if __name__ == '__main__':
     fizzbuzz()
